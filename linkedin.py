59f65d9f46c9e439c2ad64395921b771ff6f3bac
Səmədov İsmət <ismetsemedov@gmail.com>
2023-09-07 20:15:49 +0400

    check

--- linkedin.py
+++ linkedin.py
@@ -8,8 +8,8 @@ o = {}
 k = []
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
-target_url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Java%20%28Programming%20Language%29&location=Las%20Vegas%2C%20Nevada%2C%20United%20States&geoId=100293800&currentJobId=3415227738&start={}'
-for i in range(0, math.ceil(117 / 25)):
+target_url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Data%20Analyst&location=United%20States&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=50'
+for i in range(0, math.ceil(190000/50)):
 
     res = requests.get(target_url.format(i))
     soup = BeautifulSoup(res.text, 'html.parser')
